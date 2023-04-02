957fce9a7788f9e7ea7740f8011c4ee14da75de8
tscabral1 <44780117+tscabral1@users.noreply.github.com>
2023-04-02 14:51:33 -0300

    Update postinstall.sh

--- openbox/postinstall.sh
+++ openbox/postinstall.sh
@@ -35,12 +35,12 @@ sudo tar -xzvf catppuccin-papirus-icons.tar.gz;
 sudo rm -r catppuccin-papirus-icons.tar.gz;
 
 #Install trackpad gestures
-cd;
-git clone https://github.com/bulletmark/libinput-gestures.git;
-cd libinput-gestures;
-sudo ./libinput-gestures-setup install;
-cd;
-sudo rm -r libinput-gestures;
-sudo gpasswd -a $USER input;
-newgrp input;
+cd &&
+git clone https://github.com/bulletmark/libinput-gestures.git &&
+cd libinput-gestures &&
+sudo ./libinput-gestures-setup install &&
+cd &&
+sudo rm -r libinput-gestures &&
+sudo gpasswd -a $USER input &&
+newgrp input &&
 libinput-gestures-setup desktop autostart start status
